9caa8a7574428ed2035f755ab398c964b54fbeb3
Driss <driss.belaroussi.sio@gmail.com>
2016-11-28 14:56:31 +0100

    attribut ville

--- Garagiste.java
+++ Garagiste.java
@@ -1,9 +1,11 @@
 public class Garagiste {
 		private String nom ;
 		private String adresse ;
+		private ville ; 
 		
 
-public Garagiste( String nom , String adresse ){
+public Garagiste( String nom , String adresse , String ville ){
 	this.nom = nom ;
 	this.adresse = adresse ; 
+	this.ville = ville ; 
 }
