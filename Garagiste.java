da0a313921a6d6169ae3a877d0e3ee5214818b72
Driss <driss.belaroussi.sio@gmail.com>
2016-11-28 14:26:42 +0100

    contrusteur Garagiste (string, string)

--- Garagiste.java
+++ Garagiste.java
@@ -1,4 +1,9 @@
 public class Garagiste {
 		private String nom ;
 		private String adresse ;
+		
+
+public Garagiste( String nom , String adresse ){
+	this.nom = nom ;
+	this.adresse = adresse ; 
 }
